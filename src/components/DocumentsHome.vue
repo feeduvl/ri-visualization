150b1ec3110355fd9e231cc2668b2c38a5dda0a6
Johannes Daub <polynoman@live.de>
2021-06-15 19:11:27 +0200

    Fix highlighting

--- src/components/DocumentsHome.vue
+++ src/components/DocumentsHome.vue
@@ -64,7 +64,8 @@ export default {
       let list = []
       for (let topic in this.selectedResult.topics) {
         console.log(this.selectedResult.topics[topic]);
-        for (let word in this.selectedResult.topics[topic]) {
+        for (let index in this.selectedResult.topics[topic]) {
+          let word = this.selectedResult.topics[topic][index];
           console.log(word);
           console.log(!(list.indexOf(word) > -1));
           if (!(list.indexOf(word) > -1)) {
