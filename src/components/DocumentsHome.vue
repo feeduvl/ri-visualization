752cbe84ae2d1548552feca74740adf334da8522
Johannes Daub <polynoman@live.de>
2021-06-15 18:47:18 +0200

    Add debug print

--- src/components/DocumentsHome.vue
+++ src/components/DocumentsHome.vue
@@ -63,7 +63,10 @@ export default {
     topicWordlist() {
       let list = []
       for (let topic in this.selectedResult.topics) {
+        console.log(topic);
         for (let word in topic.words) {
+          console.log(word);
+          console.log(!(list.indexOf(word) > -1));
           if (!(list.indexOf(word) > -1)) {
             list.push(word);
           }
