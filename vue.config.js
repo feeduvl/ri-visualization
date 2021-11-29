b70665b0993543f5d04070122d4d0f9640c7fd0d
Michael Anders <40489366+manders123@users.noreply.github.com>
2021-11-29 13:31:31 +0100

    Add devtools

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
-Vue.config.devtools = true
-
 module.exports = {
   publicPath: '/dashboard/',
   productionSourceMap: false,
   devServer:{
     disableHostCheck: true,
   }
+  Vue.config.devtools = true
 };
