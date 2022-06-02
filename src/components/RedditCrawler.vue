08d7667ef265a4b6ed1ddb7a52f6a5b6f2513bc4
Johannes Wenzel <jonzel@t-online.de>
2022-06-02 11:31:11 +0200

    added logging for scheduling

--- src/components/RedditCrawler.vue
+++ src/components/RedditCrawler.vue
@@ -309,7 +309,7 @@
             replaceURLS: false,
             replaceEmojis: false,
             schedule: false,
-            reoccurance_days: 0,
+            occurrence_days: 0,
 
             tableHeaders: [
                     {
@@ -395,15 +395,17 @@
             this.$store.dispatch("actionCrawlReddit", crawlerTaskString)
 
             // store crawler job
+            console.log("occurrence: "+this.schedule)
             if (this.schedule) {
                 const diffTime = Math.abs(this.dateTo - this.dateFrom);
-                this.reoccurance_days = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+                this.occurrence_days = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+                console.log(this.occurrence_days)
             }
 
             let crawlerTaskInDB = {
                 subreddit_names: this.subredditNamesChips.join(','),
                 date: new Date(),
-                occurrence: this.reoccurance_days,
+                occurrence: this.occurrence_days,
                 number_posts: 0,
                 dataset_name: this.collectionNamesChips[0],
                 request: crawlerTaskString
