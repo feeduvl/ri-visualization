25650d356958526f8ae1d89a6a4d81b04e6c5212
Anh Tu Duong Nguyen <anhtuduong.nguyen@outlook.de>
2022-11-22 22:31:18 +0100

    add endpoints for app review crawler

--- src/RESTconf.js
+++ src/RESTconf.js
@@ -153,3 +153,11 @@ export const POST_CRAWLER_DATA_ENDPOINT = `${BASE_URL}/hitec/repository/concepts
 export const DELETE_CRAWLER_JOB_ENDPOINT = function(date){
   return `${BASE_URL}/hitec/repository/concepts/store/reddit_crawler/jobs/${date}`
 };
+
+// app review crawler 
+export const APP_REVIEW_CRAWLER_ENDPOINT = `${BASE_URL}/hitec/app/crawl`
+export const APP_REVIEW_GET_JOBS_ENDPOINT = `${BASE_URL}/hitec/repository/concepts/crawler_jobs/all`
+export const POST_APP_REVIEW_CRAWLER_DATA_ENDPOINT = `${BASE_URL}/hitec/repository/concepts/store/app_review_crawler/jobs`
+export const DELETE_APP_REVIEW_CRAWLER_JOB_ENDPOINT = function(date){
+  return `${BASE_URL}/hitec/repository/concepts/store/app_review_crawler/jobs/${date}`
+};
