84864f4fbb12b51c8453db905feb959bfacda3a8
dkeyGit <flip@knorr-online.de>
2023-01-18 17:04:22 +0100

    fix delete code bug- try and error

--- src/components/annotator/Annotator.vue
+++ src/components/annotator/Annotator.vue
@@ -210,7 +210,7 @@
                        isLinking: isLinking,
                        algo_lemma: $store.state.selected_algo_result !== null && $store.getters.lemmasFromSelectedResult.includes($store.state.tokens[selected_doc.begin_index + (tokensPerPage * (selectedPage - 1)) + token_number - 1].lemma?$store.state.tokens[selected_doc.begin_index + (tokensPerPage * (selectedPage - 1)) + token_number - 1].lemma.toLowerCase():''),
                        show_tore: false,
-                       toreClass: "",
+                       toreClass: '',
                        show_pos: $store.state.tokens[selected_doc.begin_index + (tokensPerPage * (selectedPage - 1)) + token_number - 1].pos!==null && $store.state.selected_pos_tags.includes($store.state.tokens[selected_doc.begin_index + (tokensPerPage * (selectedPage - 1)) + token_number - 1].pos),
                        posClass: $store.state.tokens[selected_doc.begin_index + (tokensPerPage * (selectedPage - 1)) + token_number - 1].pos,
                        annotatorInputVisible: $store.state.annotatorInputVisible
