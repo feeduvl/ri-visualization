04c914098d08df39711315d7c17969d9d12b3948
Anh Tu Duong Nguyen <anhtuduong.nguyen@outlook.de>
2023-02-02 20:41:50 +0100

    fix typo

--- src/components/AppReviewCrawler.vue
+++ src/components/AppReviewCrawler.vue
@@ -349,12 +349,12 @@
                 var date_From = this.date_from;
                 date_From = new Date(date_From.split('/')[2], date_From.split('/')[1]-1, date_From.split('/')[0]);
                 var date_To = this.date_to;
-                date_To = new Date(date_To.split('/')[2], date_To.split('/'[1]-1, date_To.split('/')[0]))
+                date_To = new Date(date_To.split('/')[2], date_To.split('/')[1]-1, date_To.split('/')[0]);
                 const diffTime = Math.abs(new Date(date_To.getTime() - date_From.getTime()));
                 this.occurrence_days = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
                 console.log(this.occurrence_days)
             }
-
+            
             let crawlerTaskInDB = {
                 app_name:this.appName,
                 app_url: this.appURL,
