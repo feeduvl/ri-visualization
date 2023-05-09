0c611ec12ac99946bde53738bd90b738136222e8
Benjamin Tuna <benjamintuna@gmail.com>
2023-05-09 21:28:51 +0200

    test jira dashboard

--- src/jira-service.js
+++ src/jira-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const ISSUE_API_BASE_URL ='http://localhost:8080/hitec/jira/issues'
+const ISSUE_API_BASE_URL ='http://feed-uvl.ifi.uni-heidelberg.de/hitec/jira/issues'
 
 class IssuesService {
 
