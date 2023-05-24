64938d69595579afdf1f1ac5aa2fbd4e3668ac61
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2023-05-24 16:58:35 +0200

    updates webpack config to resolve imports better

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,15 @@ module.exports = {
   output: {
     path: path.join(__dirname, "public"),
     filename: "bundle.js",
-    publicPath: "/dashboard/",        
+    publicPath: "/dashboard/",
   },
   devServer: {
     historyApiFallback: true
-  }
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      '@': path.resolve('src'),
+    }
+  },
 };
