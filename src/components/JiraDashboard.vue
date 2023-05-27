2abcde7abeff449bff1fe35653de336a6b451baa
Benjamin Tuna <benjamintuna@gmail.com>
2023-05-27 16:37:26 +0200

    dropdown for project names

--- src/components/JiraDashboard.vue
+++ src/components/JiraDashboard.vue
@@ -12,7 +12,7 @@
                         label="Select project" item-text="name" style="margin-left: 15px"
                         :disabled="searchForProject === '1'"></v-select>
                     <v-radio value="0"></v-radio>
-                    <v-text-field v-model="projectNameBySearch" append-icon="mdi-magnify" label="type project name ..."
+                    <v-text-field v-model="projectNameBySearch" append-icon="mdi-magnify" label="type project key ..."
                         style="margin-left: 30px; width: 30%" :disabled="searchForProject === '0'">
                     </v-text-field>
                     <v-radio value="1"></v-radio>
@@ -76,14 +76,24 @@
                     </v-data-table>
                 </v-card>
                 <v-btn dark color="blue" @click="importSelectedIssues()" style="margin-left: 65%">Import</v-btn>
-                <v-btn dark color="blue" @click="addSelectedIssues()">Add</v-btn>
+                <v-btn dark color="blue" @click="addSelectedIssues()">Add to existing</v-btn>
                 <v-btn dark color="black" @click="closeDialogIssues()">Close</v-btn>
             </div>
         </v-dialog>
         <div>
             <v-card class="v-card">
                 <v-card-title>
-                    <v-text-field v-model="search" append-icon="search" label=" Search in table..."></v-text-field>
+                    <div style="width: 40%">
+                        <v-select
+                            v-model="filterProjectName"
+                            :items="projectNames"
+                            label="Filter by project name"
+                            item-text="name"
+                        ></v-select>
+                    </div>
+                    <div style="width: 50%; margin-left: 25px">
+                        <v-text-field v-model="search" append-icon="search" label=" Search in table..."></v-text-field>
+                    </div>
                 </v-card-title>
                 <v-data-table :headers="headers" :items="getIssues" item-key="key" class="elevation-1"
                     :total-items="totalItems" rows-per-page-text="Issues per page"
@@ -121,6 +131,7 @@ export default {
             issueTypes: [],
             issuesToImportOrAdd: [],
             search: "",
+            filterProjectName: "",
             totalItems: 0,
             projectNameBySearch: "",
             projectNameBySelect: "",
@@ -229,11 +240,22 @@ export default {
                 item
             }));
         },
-        getIssues() {
-            if (this.search === "") {
-                return this.issues
-            } else {
+        getIssues(){
+            if(this.filterProjectName !== "" && this.filterProjectName !== "-"){
+                console.log(this.filterProjectName)
+                if(this.search !== ""){
+                    console.log(this.search)
+                    return this.filterIssues
+                }
+                return this.filterIssuesByProjectName
+            }
+            else if(this.search !== ""){
+                this.getAllIssues()
                 return this.filterIssues
+            }
+            else{
+                this.getAllIssues()
+                return this.issues
             }
         },
         getIssuesToSelect() {
@@ -250,6 +272,12 @@ export default {
                     || item.issueType.toLowerCase().indexOf(this.search.toLowerCase()) > -1
                     || item.projectName.toLowerCase().indexOf(this.search.toLowerCase()) > -1
             })
+        },
+        filterIssuesByProjectName(){
+            this.issues = this.issues.filter(item =>{
+                return item.projectName.toLowerCase().indexOf(this.filterProjectName.toLowerCase()) > -1
+            })
+            return this.issues
         },
         filterIssuesToSelect() {
             return this.issuesToImportOrAdd.filter(item => {
