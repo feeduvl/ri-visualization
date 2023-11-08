ccba53839d510e354120fa30793fd5d225ad68b5
tunao <benjamintuna@gmail.com>
2023-11-08 14:31:18 +0100

    removed cancel button from loading dialog

--- src/components/jiraDashboardViews/dialogs/LoadingView.vue
+++ src/components/jiraDashboardViews/dialogs/LoadingView.vue
@@ -5,20 +5,11 @@
         Loading...
       </v-progress-circular>
     </div>
-    <v-btn dark color="black" @click="cancelLoading()"
-    >Cancel
-    </v-btn>
   </div>
 </template>
 
 <script>
-export default {
-  methods:{
-    cancelLoading() {
-      this.$store.dispatch("actionCancelLoading");
-    },
-  },
-}
+
 </script>
 
 <style scoped>
